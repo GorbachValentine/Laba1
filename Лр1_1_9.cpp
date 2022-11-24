dbb1f66108225fe72cb27bb1c5685b95d3977146
GorbachValentine <118935768+GorbachValentine@users.noreply.github.com>
2022-11-24 21:10:46 +0300

    Add files via upload

---
+++ Лр1_1_9.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int n, i;
+	double a, d, sum;
+
+	puts("Введите n: ");
+	scanf_s("%d", &n);
+
+	puts("Введите a: ");
+	scanf_s("%lf", &a);
+
+	puts("Введите d: ");
+	scanf_s("%lf", &d);
+
+	for (sum = 0, i = 1; i <= n; i++)
+		sum += a + (i - 1) * d;
+
+	printf("\nsum = %.5lf\n", sum);
+
+	return 0;
+}
